220f8957cc86cfa48af3f30626d206a00f381d2d
Sergey <example@gmail.com>
2022-09-09 18:32:47 +0300

    Урок 12

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 "use strict";
 
-let name = prompt('Your name: ','');
-alert(`Hello, ${name}`);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 0);
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false,
+};
+
+let movies = {
+	lastWatchedFilm: '',
+	grade: '',
+};
+for(let i = 0; i < 2; i++) {
+	const lastWatchedFilm = prompt('Один из последних просмотренных фильмов?', '');
+	const grade = +prompt('На сколько оцените его?', '');
+	personalMovieDB.movies[lastWatchedFilm] = grade;
+}
+
+console.log(personalMovieDB);
