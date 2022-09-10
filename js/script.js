27d10a52d23d72a185f5531e6e3e1e9d07e10bc5
Sergey <example@gmail.com>
2022-09-10 15:02:34 +0300

    Урок 20 НЕ ДЗ

--- js/script.js
+++ js/script.js
@@ -1,85 +1,30 @@
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-}
-
-let personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	private: false,
+const person = {
+	name: "Sergey",
+	age: 19,
+	clothes: {
+		top: 'sweater',
+		bottom: 'shorts'
+	}
 };
 
-function rememberMyFilms() {
-	for(let i = 0; i < 2; i++) {
-		let checkFirstQ = true;
-		let checkSecondQ = true;
-		let lastWatchedFilm;
-		let grade;
+for(let key in person) {
+	if(typeof (person[key]) === 'object') {
+		for(let i in person[key]) {
+			console.log(`Key: ${i} || Value: ${person[key][i]}`);
 
-		while(checkFirstQ) {
-			lastWatchedFilm = prompt('Один из последних просмотренных фильмов?', '');
-			if(lastWatchedFilm != null && lastWatchedFilm != '' && lastWatchedFilm.length <= 50) {
-				checkFirstQ = false;
-			}
 		}
-
-		while(checkSecondQ) {
-			grade = +prompt('На сколько оцените его?', '');
-			if(grade != null && grade != '') {
-				checkSecondQ = false;
-			}
-		}
-
-		personalMovieDB.movies[lastWatchedFilm] = grade;
-	}
-}
-
-function detectPersonalLevel() {
-	if(personalMovieDB['count'] < 10) {
-		console.log('Просмотрено довольно мало фильмов');
-	} else if(personalMovieDB['count'] >= 10 && personalMovieDB['count'] < 30) {
-		console.log('Вы классический зритель');
-	} else if(personalMovieDB['count'] >= 30) {
-		console.log('Вы киноман');
 	} else {
-		console.log('Произошла ошибка');
+		console.log(`Key: ${key} || Value: ${person[key]}`);
 	}
 }
 
-console.log(personalMovieDB);
+console.log(Object.keys(person).length);
 
-function showMyDB(privat) {
-	if(!privat) {
-		start();
-	}
-}
+const {
+	top,
+	bottom
+} = person.clothes;
 
-showMyDB(personalMovieDB.private);
-
-function writeYourGenres() {
-	for(let i = 1; i <= 3; i++) {
-		const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-		personalMovieDB.genres[i - 1] = genre;
-	}
-	return personalMovieDB.genres;
-}
-
-console.log(writeYourGenres());
+console.log(top);
