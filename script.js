5cbafac4f1c25e927a3c648b689eb06fa75137d4
Sergey <example@gmail.com>
2022-09-12 18:17:05 +0300

    Урок 31

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach(btn => {
+	btn.addEventListener('mouseenter', function(event) {
+		event.target.style.cssText = `background-color: ${getRgb()}`;
+	});
+});
+
+function getRgb() {
+	const r=Math.floor(Math.random() * 255);
+	const g=Math.floor(Math.random() * 255);
+	const b=Math.floor(Math.random() * 255);
+	return `rgb(${r},${g},${b})`;
+}
